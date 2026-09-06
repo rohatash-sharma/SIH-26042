f492e19c7c56565ba2bb23cda320800e487c37c8
shreyas25869 <kirito25869@gmail.com>
2026-09-06 19:47:21 +0530

    Add seed content files for Person 2

---
+++ src/data/seed/seedLessons.js
@@ -0,0 +1,14 @@
+// SIH 26042 - Seed Lessons
+
+const seedLessons = [
+  // Add lesson seed objects here.
+  // Example:
+  // {
+  //   id: "evs-water-lesson-1",
+  //   chapterId: "evs-water",
+  //   title: "Sources of Water",
+  //   content: "",
+  // }
+];
+
+export default seedLessons;
